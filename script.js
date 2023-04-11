cc201fc7d70c310a77983a258bcdb548fb7c58c8
RAHUL <rahulraveendrannp@gmail.com>
2023-04-11 09:24:08 +0530

    changed javascript

--- script.js
+++ script.js
@@ -72,3 +72,56 @@ $(document).ready(function () {
     });
   });
   
+  $("#submit-form").submit((e)=>{
+      e.preventDefault()
+      $.ajax({
+          url:"https://script.google.com/macros/s/AKfycbzzp2IdwKetbOa3pYgG1eb_H4x710_5UZhhRwLfXMHFEckHm9RENk7gE6mZrw9ktE2p/exec",
+          data:$("#submit-form").serialize(),
+          method:"post",
+          success:function (response){
+            Swal.fire({
+              title: 'I will get back to you soon. ...',
+          
+              icon: 'sucess',
+              
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'OK'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire(
+                  window.location.reload()
+                )
+              }
+            })
+              
+              
+          },
+          error:function (err){
+            Swal.fire('Error')
+
+          }
+      })
+  })
+
+  const form = document.getElementById('submit-form'),
+        name =document.getElementById('name'),
+        email  =document.getElementById('email'),
+        subject =document.getElementById('subject'),
+        message =document.getElementById('text')
+
+function sendMail(e){
+  e.preventDefault();
+       
+        Email.send({
+          Host : "smtp.elasticemail.com",
+          Username : "rahulraveendrannp@gmail.com",
+          Password : "C3759256C03A638D6DE87494456640D89045",
+          To : 'rahulraveendrannp@gmail.com',
+          From : 'rahulraveendrannp1996@gmail.com',
+          Subject : subject.value,
+          Body : message.value
+      }).then(
+        message => console.log(message)
+      );
+    }
